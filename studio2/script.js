e147a07ec50830480fc1964e05c7a6cd8513a31f
Jeremy-Tchao <Jeremyzao@gmail.com>
2018-02-04 23:49:12 -0800

    Studio 2

---
+++ studio2/script.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var mbp = document.getElementById('mbp');
+var ipp = document.getElementById('ipp');
+var fourseven = document.getElementById('fourseven');
+var close = document.getElementsByClassName('close');
+var mbpd = document.getElementById('mbpd');
+var ippd = document.getElementById('ippd');
+var keychaind = document.getElementById('foursevend');
+
+document.getElementById("bpk").onclick = enlargeAndNext;
+
+mbp.addEventListener('click', function(){
+    mbpd.className="show";
+    document.getElementById("container").className = "firstColor";
+    document.getElementById("displayarea").className = "transparent";
+    document.querySelector('footer').className = 'hide';
+  });
+
+ipp.addEventListener('click', function(){
+    ippd.className="show";
+    document.getElementById("container").className = "firstColor";
+    document.getElementById("displayarea").className = "transparent";
+    document.querySelector('footer').className = 'hide';
+});
+
+fourseven.addEventListener('click', function(){
+    keychaind.className="show";
+    document.getElementById("container").className = "firstColor";
+    document.getElementById("displayarea").className = "transparent";
+    document.querySelector('footer').className = 'hide';
+  });
+
+close[0].addEventListener('click', function(){
+mbpd.className="hide";
+document.getElementById("container").className = "secondColor";
+document.getElementById("displayarea").className = "show";
+document.querySelector('footer').className = 'show';
+});
+
+
+close[1].addEventListener('click', function(){
+ippd.className="hide";
+document.getElementById("container").className = "secondColor";
+document.getElementById("displayarea").className = "show";
+document.querySelector('footer').className = 'show';
+});
+
+close[2].addEventListener('click', function(){
+keychaind.className="hide";
+document.getElementById("container").className = "secondColor";
+document.getElementById("displayarea").className = "show";
+document.querySelector('footer').className = 'show';
+});
+
+function enlargeAndNext()
+{   
+    
+    document.getElementById("bpki").className = "transparent";
+    document.getElementById("container").className = "secondColor";
+    
+    
+    document.querySelector('footer').style = 'color: black';
+    document.getElementsByClassName("footera")[0].style.color = "black";
+    document.getElementsByClassName("footera")[1].style.color = "black";    
+
+    
+    setTimeout(function(){document.getElementById("bpk").className = "hide";}, 1000);
+    setTimeout(function(){document.getElementById("items").className = "show";}, 800);
+    
+    
+    
+}
+
+
+
+
